768e4f24b96e40cdd6ed1b065781e3f0c003a238
Joshua Miller <joshuadanielmiller@outlook.com>
2023-10-06 20:10:39 -0500

    Fleshing out Vercel servless hosting

--- api/README.md
+++ api/README.md
@@ -7,3 +7,9 @@ For more information, check out this link: https://vercel.com/docs/functions/ser
 ## Vercel Hook
 
 `vercel_hook.go` is served at the following address: https://wip.cloudpups.dev/api/vercel_hook
+
+## Necessary Environment Variables
+
+* `app_id` = the App ID from the GitHub App Registration
+* `webhook_secret_key` = the Webhook Secret from the GitHub App Registration
+* `private_key` = a Private Key from the GitHub App Registration (simply copy+paste the content of the private key)
--- api/vercel_hook.go
+++ api/vercel_hook.go
@@ -1,10 +1,30 @@
 package vercel
- 
+
 import (
-  "fmt"
-  "net/http"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/v55/github"
 )
- 
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	webhookSecretKey := os.Getenv("webhook_secret_key")
+
+	if webhookSecretKey == "" {
+		return
+	}
+
+	payload, err := github.ValidatePayload(r, []byte(webhookSecretKey))
+	if err != nil {
+		return
+	}
+
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
+
+	if err != nil {
+		return
+	}
+
+	event = event.(github.PullRequestEvent)
+
 }
